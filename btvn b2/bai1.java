d5af64b34dd164eb1de980ebdfe0edebd4e581bf
mnguyet <minhnguyet2022005@gmail.com>
2025-06-13 09:49:10 +0700

    NHOM 10

--- btvn b2/bai1.java
+++ btvn b2/bai1.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Nhập bán kính r: ");
-        double r = s.nextDouble();
-        if(0<r & r<1000) {
-            double c = 2 * 3.14 *r;
-            double S = 3.14 * r * r;
-            System.out.printf("Chu vi của hình tròn là: %.3f\n", c);
-            System.out.printf("dien tich của hình tròn là: %.3f\n",S);
-        }
+        double a,b,c;
+        System.out.print("Nhập 3 so a,b,c: "+ " ");
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        double c = s.nextDouble();
     }
 }
